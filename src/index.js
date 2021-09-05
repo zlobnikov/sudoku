df13630807d2501102bc241747efa1bd2a622f00
zlobnikov <daffodil@list.ru>
2021-09-05 20:10:51 +0300

    feat: find candidate instead of remember

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = function solveSudoku(sudoku) {
 
   let knownNumberCounter = 0;
   let prevCycleCounter;
-  let guessCandidateCoords;
 
   while (knownNumberCounter < 81) {
     knownNumberCounter = 0;
@@ -16,7 +15,7 @@ module.exports = function solveSudoku(sudoku) {
           continue;
         }
 
-        if (matrix[y][x] === undefined) return null;
+        if (matrix[y][x] === undefined) return null;  // TEMPORARY PATCH
 
         // further only unknown numbers are welcome
 
@@ -34,16 +33,21 @@ module.exports = function solveSudoku(sudoku) {
 
         const overweight = collectOverweight(matrix, y, x);
         getRidOf(matrix[y][x], overweight);
-
-        guessCandidateCoords = [y, x];
       }
     }
 
     if (knownNumberCounter === prevCycleCounter) {
-      const [y, x] = guessCandidateCoords;
-      const storage = matrix[y][x];
+      const coords = getCandidateCoords(matrix);
 
-      if (typeof storage !== 'object') continue;
+      if (coords === null) {
+        if (check(matrix)) {
+          return matrix;
+        } else {
+          return null;
+        };
+      }; // for incoming incorrect matrix
+      const [y, x] = coords;
+      const storage = matrix[y][x];
 
       matrix[y][x] = [...storage].pop();
 
@@ -51,6 +55,7 @@ module.exports = function solveSudoku(sudoku) {
 
       if (!check(draft)) {
         storage.delete(matrix[y][x]);
+        if (storage.size === 0) return null; /////////////////////
         matrix[y][x] = storage;
       }
 
@@ -77,7 +82,6 @@ function cloneSudoku(sudoku) {
   return matrix;
 }
 
-
 function collectOverweight(matrix, y, x) {
   const overweight = [];
 
@@ -93,11 +97,21 @@ function collectOverweight(matrix, y, x) {
   return overweight;
 }
 
-
 function getRidOf(sequence, overweight) {
   overweight.forEach(value => sequence.delete(value));
 }
 
+function getCandidateCoords(matrix) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < matrix[y].length; ++x) {
+      if (typeof matrix[y][x] === 'object') {
+        return [y, x];
+      }
+    }
+  }
+
+  return null;
+}
 
 function check(draft) {
   if (!draft) return false;
@@ -105,6 +119,13 @@ function check(draft) {
   for (let i = 0; i < draft.length; ++i) {
     if (( new Set(draft[i]) ).size !== 9) return false;
     if (( new Set(draft.map(row => row[i])) ).size !== 9) return false;
+
+    const y = Math.floor(i / 3) * 3;
+    const x = i % 3 * 3;
+
+    if ((new Set(draft.slice(y, y + 3).reduce(
+      (all, row) => all.concat(row.slice(x, x + 3)), []
+    ))).size !== 9 ) return false;
   }
 
   return true;
